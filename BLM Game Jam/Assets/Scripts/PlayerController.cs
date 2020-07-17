733b4a289cbca196abec305f78b1e35d17d5728a
Cindy Pham <cindy619mysterio@gmail.com>
2020-07-16 21:48:07 -0500

    Finish scripting player movement

---
+++ BLM Game Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Player's movement speed
+    public float speed;
+    // Player's jump force
+    public float jumpForce;
+    // Player's Rigidbody2D component
+    private Rigidbody2D rb;
+
+    // Check whether player is not in the air
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the player's Rigidbody2D
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckIfGrounded();
+        Jump();
+    }
+
+    void Move() 
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float movement = x * speed;
+        rb.velocity = new Vector2(movement, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D col = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+
+        if (col != null)
+            isGrounded = true;
+        else
+            isGrounded = false;
+    }
+}
