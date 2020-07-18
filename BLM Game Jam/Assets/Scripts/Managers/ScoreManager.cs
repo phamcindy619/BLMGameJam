2c60fcd04b5f13be0557da0a4af3065a3552f25b
Cindy Pham <cindy619mysterio@yahoo.com>
2020-07-18 14:53:30 -0500

    Add end scores so game will finish

--- BLM Game Jam/Assets/Scripts/Managers/ScoreManager.cs
+++ BLM Game Jam/Assets/Scripts/Managers/ScoreManager.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
+    private Dictionary<string, int> endScores;
     private Dictionary<string, int> scores;
 
     private Dictionary<string, TextMeshProUGUI> textMeshes;
@@ -15,6 +16,8 @@ public class ScoreManager : MonoBehaviour
     public TextMeshProUGUI HatScore;
     public TextMeshProUGUI WaterScore;
     public TextMeshProUGUI FoodScore;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +32,15 @@ public class ScoreManager : MonoBehaviour
                 {"hat", 0},
                 {"water", 0},
                 {"food", 0},
+            };
+
+            endScores = new Dictionary<string, int>
+            {
+                {"mask", 7},
+                {"sunscreen", 15},
+                {"hat", 7},
+                {"water", 15},
+                {"food", 15},
             };
             
             textMeshes = new Dictionary<string, TextMeshProUGUI>
@@ -49,6 +61,9 @@ public class ScoreManager : MonoBehaviour
         {
             textMeshes[item.Key].text = $" x {item.Value}";
         }
+        // End game if scores reached
+        if (CheckIfFinish())
+            GameManager.instance.FinishGame();
     }
 
     public void addScore(string itemName, int incr)
@@ -56,5 +71,16 @@ public class ScoreManager : MonoBehaviour
         int value = scores[itemName.ToLower()];
         scores[itemName.ToLower()] = value + incr;
         updateScoreBoard();
+    }
+
+    // Check whether enough items are collected to finish game
+    bool CheckIfFinish()
+    {
+        foreach (KeyValuePair<string, int> item in scores)
+        {
+            if (item.Value < endScores[item.Key])
+                return false;
+        }
+        return true;
     }
 }
