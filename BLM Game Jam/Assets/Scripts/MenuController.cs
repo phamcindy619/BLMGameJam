333d015c89f4f86e3a98aa64a3a95e299129fb80
Cindy Pham <cindy619mysterio@yahoo.com>
2020-07-17 22:19:59 -0500

    Fix game over pop up

--- BLM Game Jam/Assets/Scripts/MenuController.cs
+++ BLM Game Jam/Assets/Scripts/MenuController.cs
@@ -6,22 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public Button playButton;
-    public Button quitButton;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playButton.onClick.AddListener(StartGame);
-        quitButton.onClick.AddListener(QuitGame);
-    }
-
-    void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
 
-    void QuitGame()
+    public void QuitGame()
     {
         Application.Quit();
     }
